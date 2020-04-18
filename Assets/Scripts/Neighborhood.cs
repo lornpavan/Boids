5f6c4270a44c0ca0574a9cdc31a60961123da439
lornpavan <62705550+lornpavan@users.noreply.github.com>
2020-04-17 23:34:05 -0400

    Initial commit

---
+++ Assets/Scripts/Neighborhood.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighborhood : MonoBehaviour {
+[Header("Set Dynamically")]
+public List<Boids> neighbors;
+private SphereCollider coll;
+
+// Use this for initialization
+void Start () {
+	neighbors = new List<Boids>();
+	coll = GetComponent<SphereCollider> ();
+	coll.radius = Spawner.S.neighborDist / 2;
+}
+
+
+// Update is called once per frame
+void FixedUpdate () {
+	if (coll.radius != Spawner.S.neighborDist / 2) {
+									coll.radius = Spawner.S.neighborDist / 2;
+	}
+}
+
+
+/* if a Boid enters my zone add to neighbors list */
+void OnTriggerEnter(Collider other){
+	Boids b = other.GetComponent<Boids>();
+	if (b != null)
+									if (neighbors.IndexOf (b) == -1) {
+																	neighbors.Add (b);
+									}
+}
+
+
+/* if a Boid leave my zone remove it from the neighbors list */
+void OnTriggerExit(Collider other){
+	Boids b = other.GetComponent<Boids> ();
+	if (b != null)
+									if (neighbors.IndexOf (b) == -1) {
+																	neighbors.Remove(b);
+									}
+}
+
+
+public Vector3 avgPos {
+	get {
+		Vector3 avg = Vector3.zero;
+		if (neighbors.Count == 0)
+			return avg;
+	    for (int i = 0; i < neighbors.Count; i++) {
+		    avg += neighbors [i].pos;
+		}
+		avg /= neighbors.Count;
+		return avg;
+	}
+}
+
+public Vector3 avgVel {
+	get {
+	    Vector3 avg = Vector3.zero;
+		if (neighbors.Count == 0)
+			return avg;
+		for (int i = 0; i < neighbors.Count; i++) {
+			avg += neighbors[i].rigid.velocity;
+		}
+		avg /= neighbors.Count;
+
+		return avg;
+		}
+}
+
+public Vector3 avgClosePos {
+	get {
+		Vector3 avg = Vector3.zero;
+		Vector3 delta;
+		int nearCount = 0;
+		for (int i = 0; i < neighbors.Count; i++) {
+			delta = neighbors [i].pos - transform.position;
+			if (delta.magnitude <= Spawner.S.collDist) {
+				avg += neighbors [i].pos;
+				nearCount++;
+			}
+		}
+		// if there are no neighbors too close. return Vector3.zero
+	    if (nearCount == 0)
+			return avg;
+        // otherwise
+		avg /= nearCount;
+		return avg;
+								}
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+static public Spawner S;
+static public List<Boids> boids;
+
+[Header("Set in Inspector: Spawning")]
+public GameObject boidsPrefab;
+public Transform boidsAnchor;
+public int numBoids = 100;
+public float spawnRadius = 100f;
+public float spawnDelay = 0.1f;
+
+// adjust the flocking behavoir of the Boids
+public float velocity = 30f;
+public float neighborDist = 30f;
+public float collDist = 4f;
+public float velMatching = 0.25f;
+public float flockCentering = 0.2f;
+public float collAvoid = 2f;
+public float attractPull = 2f;
+public float attractPush = 2f;
+public float attractPushDist = 5f;
+
+void Awake () {
+	S = this;
+	boids = new List<Boids>();
+	InstantiateBoids();
+}
+
+public void InstantiateBoids(){
+	GameObject go = Instantiate (boidsPrefab);
+	Boids b = go.GetComponent<Boids> ();
+	b.transform.SetParent (boidsAnchor);
+	boids.Add (b);
+	if (boids.Count < numBoids) {
+    	Invoke ("InstantiateBoids", spawnDelay);
+	}
+}
+
+// Use this for initialization
+void Start () {
+
+}
+
+// Update is called once per frame
+void Update () {
+
+}
+}
